d91d4a263ca01cb234449ddd81a06b287df0f4f7
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-07 20:53:40 +0530

    [NIJAM] Add:All Email Validation Eg.nks.nks@gmail.com.in

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace EmailPincodeValidation
             Console.Write("Enter Pincode Only Numbers : ");
             string pincode = Console.ReadLine();
             Console.WriteLine(val.PincodeVal(pincode));
+            Console.Write("Enter E-mail : ");
+            string email = Console.ReadLine();
+            Console.WriteLine(val.EmailVal(email));
         }
     }
 }
--- Validation.cs
+++ Validation.cs
@@ -8,11 +8,18 @@ namespace EmailPincodeValidation
     class Validation
     {
         public static string pin = "^[0-9]{3}[ ][0-9]{3}$";
+        public static string EMAIL_PATTERN = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*(\\@)[a-zA-Z0-9]+(\\.)[a-z]{2,3}[.(a-z)]*$";
         public bool PincodeVal(string pincod)
         {
             Regex Pn = new Regex(pin);
             bool Validate = Pn.IsMatch(pincod);
             return Validate;
+        }
+        public bool EmailVal(string email)
+        {
+            Regex rg = new Regex(EMAIL_PATTERN);        
+            bool validate = rg.IsMatch(email);
+            return validate;
         }
     }
 }
