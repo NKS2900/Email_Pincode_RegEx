e45226f7977dd5eed25af0bd96f50b3005b6d09e
unknown <nksayyad2900.com>
2020-12-07 09:10:37 +0530

    [NIJAM] Add:Pincode validation Six digit pincode or not.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,14 @@ namespace EmailPincodeValidation
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("***Welcome To Email_Pincode Validation***");
+            Validation val = new Validation();
+            Console.Write("Enter Pincode : ");
+            string pincode = Console.ReadLine();
+            Console.WriteLine(val.PincodeVal(pincode));
         }
     }
 }
---
+++ Validation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EmailPincodeValidation
+{
+    class Validation
+    {
+        public static string pin = "^[0-9]{6}$";
+        public bool PincodeVal(string pincod)
+        {
+            Regex Pn = new Regex(pin);
+            bool Validate = Pn.IsMatch(pincod);
+            return Validate;
+        }
+    }
+}
